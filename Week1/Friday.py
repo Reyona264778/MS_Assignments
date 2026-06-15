267247c92f06d484bc99f3b3e029872cab2d151e
Reyona264778 <264778@ust.com>
2026-06-15 11:44:38 +0530

    till match

--- Week1/Friday.py
+++ Week1/Friday.py
@@ -7,3 +7,74 @@
 #           .
 #           .
 # Microwave = Microwave()  #---> Class instantiation type nanetation is not included
+
+
+
+
+# class Dog:
+
+#     def __init__(self,name,breed,owner):
+#         self.name = name
+#         self.breed = breed
+#         self.owner = owner
+#     def bark(self):
+#         print("Woof woof")
+
+# class Owner:
+#     def __init__(self,name,address,cont):
+#         self.name = name
+#         self.address = address
+#         self.cont_no= cont
+
+
+# owner1 = Owner("Danny","Abc","1234567890")
+# d1=Dog("daash","dash",owner1)
+# print(d1.owner.name)
+# # d1.bark()
+
+
+
+
+
+# class Person:
+#     def __init__(self,name,age):
+#         self.name = name
+#         self.age = age
+
+#     def greet(self):
+#         print(f"Hello, my name is {self.name} and I am {self.age} years old")
+
+# p1 = Person("Alice",30)
+# p1.greet()
+# p2 = Person("bob",38)
+# p2.greet()
+
+
+#Accessing and modifying data
+#1. The traditional way: make the data private and use getters and setters:
+
+
+
+class User:
+    def __init__(self,username, email, password):
+        self.uname = username
+        self.__email = email      #private
+        self.pswd = password
+
+    def get_email(self):
+        return self.__email.lower().strip()
+
+    # def introo(self, user):
+    #     print(f"Hi {user.uname} this is {self.uname} welcome to my page!")
+
+u1 = User("Alice","alice@gmail.com","123")
+u2 = User("Bob","bob@gmail.com","abc")
+
+
+# print(u1.__email)
+print(u1.get_email())
+u1.__email = "hdgfhdfghgf"
+print(u1.get_email())
+
+#The "Consenting Adults" Philosophy
+
--- Week1/Thu11_06.py
+++ Week1/Thu11_06.py
@@ -19,12 +19,19 @@
 # comp1 = Computer(val)
 # comp1.config()
 
+###################################################################################################
 
 
-val = int(input("Entre the limit:"))
-rev=0
-while val!=0:
-    dig = val%10
-    rev = rev*10+dig
-    val = val//10
-print(rev)
+# val = int(input("Entre the limit:"))
+# rev=0
+# while val!=0:
+#     dig = val%10
+#     rev = rev*10+dig
+#     val = val//10
+# print(rev)
+
+###################################################################################################
+
+
+
+
