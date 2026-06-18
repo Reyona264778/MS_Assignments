bf425adb97f424a4cbdc4950edf43b3fdddccaa7
Reyona264778 <264778@ust.com>
2026-06-18 20:13:51 +0530

    Tuple

---
+++ Assignment/DataStructures/Tuple/1_Basic.py
@@ -0,0 +1,19 @@
+tuple1 = (1, 2, 3)                #
+tuple2 = ("apple", "banana", "cherry") 
+tuple3 = (1, "hello", 3.5, True)  
+
+
+
+print(tuple1[0])   
+print(tuple2[1])   
+print(tuple2[-1]) 
+
+print(tuple1[0:2])
+print(len(tuple3)) 
+
+nested = (1, (2, 3), 4)
+print(nested[1][0])
+
+
+a, b, c = (10, 20, 30)
+print(a, b, c) 
