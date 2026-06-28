e40287fa5ac841c662aea166ba4aac348743536f
Reyona264778 <264778@ust.com>
2026-06-28 16:27:06 +0530

    17th prohram

---
+++ Python_second_set_v02/17_Age_calculator.py
@@ -0,0 +1,15 @@
+birth = (1995, 5, 15)
+today = (2026, 6, 28)
+
+year = today[0] -birth[0]
+month = today[1] - birth[1]
+days = today[2] - birth[2]
+
+if days<0:
+    month -=1
+    days+=31
+if month < 0:
+    year -=1
+    month +=12
+
+print(f"{year} years {month} months {days} days old")
